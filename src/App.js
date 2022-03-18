0c206891d041da91961361f4efc69a8232c9ff28
Danielle Souza <dany.sa1@hotmail.com>
2022-03-17 22:18:07 -0300

    feat: consumindo a api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-
-import './App.css';
+import React, { useEffect } from 'react'
+import styles from './App.module.css';
+import { useState } from 'react';
+import Tasks from './Components/Tasks';
 
 function App() {
+  const [tasks, setTasks] = useState();
+
+  useEffect(()=>{
+
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+
+  })
+
+  console.log(tasks)
+
+
   return (
-    <p>inicio</p>
+    <>
+    <div className={styles.container}>
+      <Tasks tasks={tasks}/>
+    </div>
+    </>
   )
 }
 
---
+++ src/Components/Tasks.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Tasks({tasks}) {
+    console.log(tasks)
+  return (
+    <div>
+       {/* {tasks.map((task)=><p>{task.userId}</p>)}  */}
+    </div>
+  )
+}
